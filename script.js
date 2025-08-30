d769876cb3280c727c73148bfaa71c1f4d6b4eb3
Rizki Fadil <rixkifadil@gmail.com>
2025-08-30 10:27:51 +0700

    Add klik sembarang = close

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ btn.addEventListener("click", function () {
 silang.addEventListener("click", function () {
   popUp.classList.remove("active");
 });
+
+document.body.addEventListener("click", (e) => {
+  if (!popUp.contains(e.target) && e.target !== btn) {
+    popUp.classList.remove("active");
+  }
+});
