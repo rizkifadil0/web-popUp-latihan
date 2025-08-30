719a3a81c357a4e4dbc92f7488814068bde924b0
Rizki Fadil <rixkifadil@gmail.com>
2025-08-30 10:40:19 +0700

    Add domcontentloaded

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const btn = document.getElementById("btn");
-const popUp = document.getElementById("popUp");
-const silang = document.getElementById("silang");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn");
+  const popUp = document.getElementById("popUp");
+  const silang = document.getElementById("silang");
 
-btn.addEventListener("click", function () {
-  popUp.classList.add("active");
-});
+  btn.addEventListener("click", function () {
+    popUp.classList.add("active");
+  });
 
-silang.addEventListener("click", function () {
-  popUp.classList.remove("active");
-});
-
-document.body.addEventListener("click", (e) => {
-  if (!popUp.contains(e.target) && e.target !== btn) {
+  silang.addEventListener("click", function () {
     popUp.classList.remove("active");
-  }
+  });
+
+  document.body.addEventListener("click", (e) => {
+    if (!popUp.contains(e.target) && e.target !== btn) {
+      popUp.classList.remove("active");
+    }
+  });
 });
